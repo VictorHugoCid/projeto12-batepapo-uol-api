563f2086d23ca22408634b17fed6e9c3b4e472d4
VictorCid <victorcidpereira@gmail.com>
2022-09-03 18:17:12 -0300

    set3 18h17m

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,167 +1,290 @@
 import express from "express";
 import cors from 'cors';
 import dayjs from 'dayjs';
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from 'dotenv';
+import joi from 'joi';
 
-// dayjs().format()
+dotenv.config()
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-const users = [
-    {
-        "name": "victor",
-        "id": 1661941740625
-    },
-    {
-        "name": "cid",
-        "id": 1661941746881
-    },
-    {
-        "name": "hugo",
-        "id": 1661941750313
-    }
-];
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db('batePapoUOL3')
+})
 
-const messages = [
-    {
-        "to": "Maria",
-        "text": "vai funfar sim!!!",
-        "type": "message",
-        "from": "narutin",
-        "time": "15-00-00",
-        "id": 1661968800446
-    },
-    {
-        "to": "narutin",
-        "text": "bora codar",
-        "type": "message",
-        "from": "maria",
-        "time": "15-00-01",
-        "id": 1661968801118
-    },
-    {
-        "to": "renata",
-        "text": "piratinha bolado",
-        "type": "private_message",
-        "from": "hugo",
-        "time": "15-00-09",
-        "id": 1661968809424
-    },
-    {
-        "to": "victor",
-        "text": "vai funfar sim!!!",
-        "type": "private_message",
-        "from": "renata",
-        "time": "15-00-09",
-        "id": 1661968809785
-    },{
-        "to": "Maria",
-        "text": "vai funfar sim!!!",
-        "type": "message",
-        "from": "narutin",
-        "time": "15-00-00",
-        "id": 1661968800446
-    },
-    {
-        "to": "narutin",
-        "text": "bora codar",
-        "type": "message",
-        "from": "maria",
-        "time": "15-00-01",
-        "id": 1661968801118
-    },
-    {
-        "to": "renata",
-        "text": "piratinha bolado",
-        "type": "private_message",
-        "from": "hugo",
-        "time": "15-00-09",
-        "id": 1661968809424
-    },
-    {
-        "to": "victor",
-        "text": "vai funfar sim!!!",
-        "type": "private_message",
-        "from": "renata",
-        "time": "15-00-09",
-        "id": 1661968809785
-    },
-];
+// joi objects-------------------------------------
+const userSchema = joi.object({
+    name: joi.string().required()
+})
 
-app.post('/participants', (req, res) => { //Login
-    const { name } = req.body
+const messageSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().valid("message", "private_message"),
+    time: joi.string(),
+})
 
-    if (!name) {
-        res.status(422).send({ error: 'digita alguma coisa aí, irmão' })
+// Participants ---------------------------------------------------
+
+app.post('/participants', async (req, res) => { //Login
+    const { name: username } = req.body
+
+    //validação com joi
+    const validation = userSchema.validate(req.body);
+    if (validation.error) {
+        const errors = validation.error.details.map(value => value.message)
+        res.status(422).send(errors)
         return;
     }
 
-    const isUserExist = users.find((user) => user.name === name)
-    if (isUserExist) {
-        res.status(409).send({ error: 'já tem esse nome aí, manda outro' })
-        return;
+    try {
+        const usersCollection = await db.collection('participants').findOne({ username: username });
+        if (usersCollection) {
+            res.sendStatus(409)
+            return;
+        }
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500)
     }
 
     const time = dayjs(new Date()).format('HH-mm-ss')
 
-    let message = {
-        from: name,
+    const message = {
+        from: username,
         to: 'Todos',
         text: 'entra na sala...',
         type: 'status',
         time: time,
-        id: Date.now()
     }
-    messages.push(message)
 
-    users.push({ name, id: Date.now() })  //vai virar mongo
-    res.status(201).send(users)
+    try {
+        const responsePart = await db.collection('participants').insertOne({ username: username, lastStatus: Date.now() })
+        const responseMessage = await db.collection('messages').insertOne( {...message} )
+        res.send(responsePart)
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(422)
+    }
+
 })
 
-app.get('/participants', (req, res) => {
+app.get('/participants', async (req, res) => {
 
-    res.send(users) //vai vir do mongo
+    try {
+        const response = await db.collection('participants').find().toArray();
+        res.status(201).send(response)//RETORNAR SENDSTATUS SÓ
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(404)
+    }
 })
 
-app.post('/messages', (req, res) => {
+// Messages------------------------------------------------------------------
+
+app.post('/messages', async (req, res) => {
     const userMessage = req.body
-    const { user } = req.headers
+    const { user: username } = req.headers
 
     const time = dayjs(new Date()).format('HH-mm-ss')
 
-    let message = {
+    const message = {
         ...userMessage,
-        from: user,
+        from: username,
         time: time,
-        id: Date.now(),
     }
 
-    messages.push(message)
-    res.send(messages)
+    //validação
+    // if -> from
+    const user = await db.collection('participants').findOne({ username: username })
+
+    if (!user) {
+        res.status(422).send({ error: 'Esse usuário não está logado.' })
+        return;
+    };
+
+    //joi -> text, to, type 
+
+    const validation = messageSchema.validate(message);
+    if (validation.error) {
+        const errors = validation.error.details.map(value => value.message)
+        res.status(422).send(errors)
+        return;
+    }
+
+    // insertOne
+    try {
+        const response = await db.collection('messages').insertOne( {...message} );
+        res.sendStatus(201)
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(422)
+    }
+
 })
 
-app.get('/messages', (req, res) => {
-    const { user: username } = req.headers
+app.get('/messages', async (req, res) => {
+    const { User: username } = req.headers
     const { limit } = req.query;
+    // console.log(limit)
 
-    // vou ter q pegar as mensagens com mongo
-    const showMessages = messages.filter((message) => {
-        if (message.type === 'private_message' && (message.to !== username || message.from !== username)) {
-            return false;
+    //find & showMessages
+    try {
+        const response = await db.collection('messages').find().toArray()
+        console.log(response)
+
+        const showMessages = response.filter((message) => {
+            if (message.type === 'private_message' && (message.to !== username || message.from !== username)) {
+                return false;
+            }
+            return true;
+        })
+
+        const limitMessages = showMessages.slice(0, limit)
+        res.send(limitMessages);
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+})
+
+// status-------------------------------------------------------------------------------------
+app.post('/status', async (req, res) => {
+    const { user: username } = req.headers
+
+    // verificar se o nome ta na lista
+    const userCollectionArray = await db.collection('participants').find().toArray()
+    const usernameArray = userCollectionArray.map(value => value.username)
+
+    if (!usernameArray.includes(username)) {
+        res.sendStatus(404);
+        return;
+    }
+
+    //  atualizar lastStatus
+    const user = await db.collection('participants').findOne({ username: username })
+
+    const userModel = {
+        _id: user._id,
+        username: username,
+        lastStatus: Date.now(),
+    }
+    await db
+        .collection('participants')
+        .updateOne({ username: username }, { $set: userModel })
+
+    res.sendStatus(200);
+
+})
+
+//delete--------------------------------------------------------------------
+app.delete('/participants/:id', async (req, res) => {
+    //ACHO Q ISSO AQUI ENTRA NO STATUS COM O SET INTERVAL
+    const { id } = req.params
+    console.log(id)
+
+    try {
+        const response = await db.collection('participants').deleteOne({ _id: new ObjectId(id) })
+        res.send(response)
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(404)
+    }
+
+});
+
+app.delete('/messages/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const response = await db.collection('messages').deleteOne({ _id: new ObjectId(id) })
+        res.send(response)
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(404)
+    }
+});
+
+//  editar--------------------------------------------------------------------
+
+const newMessageSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().valid("message", "private_message"),
+})
+app.put('/messages/:id', async (req, res) => {
+    const { id } = req.params
+    const { user: username } = req.headers
+
+    const newMessage = {
+        ...req.body,
+        from: username,
+    }
+
+    // // validação
+    //     // if -> from
+    // const user = await db.collection('participants').findOne({username: username})
+
+    // if (user) {
+    //     res.status(422).send({ error: 'Esse usuário não está logado.' })
+    //     return;
+    // };
+    //     // joi -> to, text, type
+
+    // const validation = newMessageSchema.validate(newMessage)
+
+    // if (validation.error) {
+    //     res.sendStatus(422)
+    //     return;
+    // }
+
+    try {
+        const message = await db.collection('messages').findOne({ _id: new ObjectId(id) })
+
+        console.log(message)
+        if (!message) {
+            res.status(404).send(message)
+            return;
         }
-        return true;
-    })
 
-    const limitMessages = showMessages.slice(0,limit)
-    res.send(limitMessages)
-})
+        if (message.from !== username) {
+            res.status(401).send(message)
+            return;
+        }
+        await db.collection('messages').updateOne({_id: new ObjectId(id)},{ $set: newMessage })
 
-app.post('/status', (req, res) => {
+        res.sendStatus(200)
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
 
-})
+});
 
+
+
+// async function verifyOnline() {
+
+//     try {
+//         const users = await db.collection('participants').find().toArray()
+//         console.log(users)
+
+//     } catch (error) {
+//         console.error(error)
+//     }
+
+
+//     const usersOut = users.map((value) => (Date.now() - users.time >= 15000))
+// }
+// verifyOnline()
 
 app.listen(PORT, () => console.log('ligou hein'));
